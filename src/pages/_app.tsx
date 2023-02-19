2897e863b1510e13859da3aef9efbb4a74a1e7b0
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2023-02-19 06:54:32 -0800

    Add Dark Mode, Login, & Firebase
    Co-authored-by: Ben Ostrovsky <Ben-Ostrovsky@users.noreply.github.com>
    Co-authored-by: Ben-ramen <BenjaminTelanoff@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,31 @@
 import type { AppProps } from "next/app";
-import { NextUIProvider } from "@nextui-org/react";
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { SSRProvider } from "react-bootstrap";
+
+const lightTheme = createTheme({
+  type: "light",
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <SSRProvider>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+    </SSRProvider>
   );
 }
